9dc853b5fb16c6abb1e688fb8c2c14fe492821d6
Kristina <kristinasimonyte@gmail.com>
2022-03-15 00:36:56 +0200

    get, post(login, register) success

--- src/server.js
+++ src/server.js
@@ -6,14 +6,57 @@ const PORT = process.env.SERVER_PORT || 3000;
 
 const app = express();
 
+const users = [
+  {
+    id: 1,
+    username: 'Mike',
+    password: 'secret',
+  },
+  {
+    id: 2,
+    username: 'James',
+    password: 'jam',
+  },
+];
+
 // middleware
 
-app.use(morgan('common'));
+app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello express');
+app.get('/users', (req, res) => {
+  res.json(users);
 });
 
+// GET users/:username (grazinti vartotoja kurio username  === username)
+app.get('/users/:username', (req, res) => {
+  const name = req.params.username;
+  const userObjFound = users.find((userObj) => userObj.username === name);
+  res.json(userObjFound);
+});
+
+app.post('/login', (req, res) => {
+  // gauti username ir password su kuriais bandoma prisiloginti
+  const { username, password } = req.body;
+  // surasti vartotoja vardu username
+  const userObjFound = users.find((userObj) => userObj.username === username);
+  // jei randam ziurim ar sutampa slaptazodziai
+  if (userObjFound && userObjFound.password === password) {
+    res.json('login success');
+  } else {
+    res.status(400).send('username or password not match');
+  }
+});
+
+app.post('/register', (req, res) => {
+  // gauti username ir password su kuriais bandoma prisiregistruoti
+  const { username, password } = req.body;
+  const newUser = {
+    username,
+    password,
+  };
+  users.push(newUser);
+  res.send('register success');
+});
 app.listen(PORT, () => console.log(`Server id running on port ${PORT}`));
